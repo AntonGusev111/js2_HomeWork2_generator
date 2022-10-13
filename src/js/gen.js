58e8356b684405c5c7bc1068311782722b08b372
GusevADresume <resumeGusevAD@gmail.com>
2022-10-13 21:32:58 +0300

    v1

---
+++ src/js/gen.js
@@ -0,0 +1,53 @@
+const char = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+  const char1 = {
+    name: 'Лучник1',
+    type: 'Bowman1',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+  const char2 = {
+    name: 'Лучник2',
+    type: 'Bowman2',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+  const char3 = {
+    name: 'Лучник3',
+    type: 'Bowman3',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+
+
+
+class Team {
+    
+    constructor(charList){
+        this.charList = charList;
+    }
+
+    *[Symbol.iterator]() {
+      for (let item = 0;  item < this.charList.length; item++){
+        yield this.charList[item];
+      }
+    }
+  }
+  
+
